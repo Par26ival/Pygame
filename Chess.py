d65201ab0503f60518b00a615567b308e030b134
Par26ival <kal.tomov@gmail.com>
2023-05-30 19:52:48 +0300

    pieces are smart
    Piece movement logic added

--- Chess.py
+++ Chess.py
@@ -66,6 +66,99 @@ def get_position_from_mouse(pos):
     col = x // square_size
     return row, col
 
+# Helper function to check if a move is valid
+def is_valid_move(start_pos, end_pos):
+    start_row, start_col = start_pos
+    end_row, end_col = end_pos
+    piece = chessboard[start_row][start_col]
+
+    # Check if the piece is moving within the bounds of the chessboard
+    if not (0 <= end_row < 8 and 0 <= end_col < 8):
+        return False
+
+    # Check if the destination is occupied by a piece of the same color
+    if chessboard[end_row][end_col] != '' and chessboard[end_row][end_col][0] == piece[0]:
+        return False
+
+    # Implement specific movement logic for each piece type
+    if piece[1] == 'p':
+        # Pawn movement logic
+        if piece[0] == 'w':
+            if start_row == 6 and start_col == end_col and start_row - end_row == 2 and chessboard[end_row + 1][end_col] == '':
+                return True
+            elif start_col == end_col and start_row - end_row == 1 and chessboard[end_row][end_col] == '':
+                return True
+            elif abs(start_col - end_col) == 1 and start_row - end_row == 1 and chessboard[end_row][end_col] != '':
+                return True
+        else:
+            if start_row == 1 and start_col == end_col and end_row - start_row == 2 and chessboard[end_row - 1][end_col] == '':
+                return True
+            elif start_col == end_col and end_row - start_row == 1 and chessboard[end_row][end_col] == '':
+                return True
+            elif abs(start_col - end_col) == 1 and end_row - start_row == 1 and chessboard[end_row][end_col] != '':
+                return True
+    elif piece[1] == 'r':
+        # Rook movement logic
+        if start_row == end_row or start_col == end_col:
+            if start_row == end_row:
+                step = 1 if start_col < end_col else -1
+                for col in range(start_col + step, end_col, step):
+                    if chessboard[start_row][col] != '':
+                        return False
+            else:
+                step = 1 if start_row < end_row else -1
+                for row in range(start_row + step, end_row, step):
+                    if chessboard[row][start_col] != '':
+                        return False
+            return True
+    elif piece[1] == 'n':
+        # Knight movement logic
+        if abs(start_row - end_row) == 2 and abs(start_col - end_col) == 1:
+            return True
+        elif abs(start_row - end_row) == 1 and abs(start_col - end_col) == 2:
+            return True
+    elif piece[1] == 'b':
+        # Bishop movement logic
+        if abs(start_row - end_row) == abs(start_col - end_col):
+            step_row = 1 if start_row < end_row else -1
+            step_col = 1 if start_col < end_col else -1
+            row, col = start_row + step_row, start_col + step_col
+            while row != end_row and col != end_col:
+                if chessboard[row][col] != '':
+                    return False
+                row += step_row
+                col += step_col
+            return True
+    elif piece[1] == 'q':
+        # Queen movement logic
+        if start_row == end_row or start_col == end_col:
+            if start_row == end_row:
+                step = 1 if start_col < end_col else -1
+                for col in range(start_col + step, end_col, step):
+                    if chessboard[start_row][col] != '':
+                        return False
+            else:
+                step = 1 if start_row < end_row else -1
+                for row in range(start_row + step, end_row, step):
+                    if chessboard[row][start_col] != '':
+                        return False
+            return True
+        elif abs(start_row - end_row) == abs(start_col - end_col):
+            step_row = 1 if start_row < end_row else -1
+            step_col = 1 if start_col < end_col else -1
+            row, col = start_row + step_row, start_col + step_col
+            while row != end_row and col != end_col:
+                if chessboard[row][col] != '':
+                    return False
+                row += step_row
+                col += step_col
+            return True
+    elif piece[1] == 'k':
+        # King movement logic
+        if abs(start_row - end_row) <= 1 and abs(start_col - end_col) <= 1:
+            return True
+    return False
+
 # Game loop
 running = True
 while running:
@@ -83,17 +176,19 @@ while running:
             else:
                 # Move the selected piece
                 row, col = get_position_from_mouse(pygame.mouse.get_pos())
-                chessboard[selected_piece_pos[0]][selected_piece_pos[1]] = ''
-                chessboard[row][col] = selected_piece
+                if is_valid_move(selected_piece_pos, (row, col)):
+                    chessboard[selected_piece_pos[0]][selected_piece_pos[1]] = ''
+                    chessboard[row][col] = selected_piece
                 selected_piece = None
                 selected_piece_pos = None
 
     screen.fill((255, 255, 255))
     draw_chessboard()
+
     if selected_piece_pos:
         pygame.draw.rect(screen, SELECTED_COLOR, (selected_piece_pos[1] * square_size, selected_piece_pos[0] * square_size, square_size, square_size))
+
     pygame.display.flip()
 
 # Quit Pygame
 pygame.quit()
-
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Pygame
- 
+
+Chess game description:
+
+Uses images
+Has piece movement logic
+
+No person vs computer mode
+No starting menu
+No timer
